62cf95f56c03fa43a019bc8eea0caa54b7a58ed3
s-nakajima <nakajimashouhei@gmail.com>
2019-08-04 23:28:55 +0900

    style: phpcsエラー修正

--- Model/CalendarPermission.php
+++ Model/CalendarPermission.php
@@ -103,7 +103,7 @@ class CalendarPermission extends CalendarsAppModel {
 	public function getCalendarRoomBlocks() {
 		//$spaceIds = array(Space::PUBLIC_SPACE_ID, Space::COMMUNITY_SPACE_ID);
 		$spaceModel = ClassRegistry::init('Rooms.Space');
-		$spaces =$spaceModel->getSpaces();
+		$spaces = $spaceModel->getSpaces();
 		foreach ($spaces as $space) {
 			// もとのコードがプライベートとサイト全体のスペース以外を取得していたのでここでも除外する
 			$spaceId = $space['Space']['id'];
